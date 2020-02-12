e60ebef69a6be66a25a727eb37fe778b680ebce5
ysc1995 <ysc1995@126.com>
2020-02-12 10:03:32 -0800

    add more starter methods to make focus more on the core logic

--- app/src/main/java/com/example/helloagorademo/VideoCallActivity.java
+++ app/src/main/java/com/example/helloagorademo/VideoCallActivity.java
@@ -8,10 +8,14 @@ import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import io.agora.rtc.IRtcEngineEventHandler;
 
 public class VideoCallActivity extends AppCompatActivity {
 
@@ -24,11 +28,45 @@ public class VideoCallActivity extends AppCompatActivity {
     private boolean isCalling = true;
     private boolean isMuted = false;
 
+    private IRtcEngineEventHandler mRtcEngineEventHandler = new IRtcEngineEventHandler() {
+        @Override
+        public void onJoinChannelSuccess(String channel, int uid, int elapsed) {
+            super.onJoinChannelSuccess(channel, uid, elapsed);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(VideoCallActivity.this, "Join channel successfully!", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public void onFirstRemoteVideoDecoded(final int uid, int width, int height, int elapsed) {
+            super.onFirstRemoteVideoDecoded(uid, width, height, elapsed);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    setUpRemoteView(uid);
+                }
+            });
+        }
+
+        @Override
+        public void onUserOffline(int uid, int reason) {
+            super.onUserOffline(uid, reason);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    removeRemoteView();
+                }
+            });
+        }
+    };
+
     // Ask for Android device permissions at runtime.
     private static final String[] REQUESTED_PERMISSIONS = {
             Manifest.permission.RECORD_AUDIO,
-            Manifest.permission.CAMERA,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
+            Manifest.permission.CAMERA
     };
 
     @Override
@@ -38,8 +76,7 @@ public class VideoCallActivity extends AppCompatActivity {
         initUI();
 
         if (checkSelfPermission(REQUESTED_PERMISSIONS[0], PERMISSION_REQ_ID) &&
-                checkSelfPermission(REQUESTED_PERMISSIONS[1], PERMISSION_REQ_ID) &&
-                checkSelfPermission(REQUESTED_PERMISSIONS[2], PERMISSION_REQ_ID)) {
+                checkSelfPermission(REQUESTED_PERMISSIONS[1], PERMISSION_REQ_ID)) {
             initEngineAndJoinChannel();
         }
     }
@@ -59,7 +96,44 @@ public class VideoCallActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQ_ID: {
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED || grantResults[1] != PackageManager.PERMISSION_GRANTED) {
+                    break;
+                }
+                initEngineAndJoinChannel();
+                break;
+            }
+        }
+    }
+
     private void initEngineAndJoinChannel() {
+        initializeEngine();
+        setUpLocalView();
+        joinChannel();
+    }
+
+    private void removeRemoteView() {
+
+    }
+
+
+    private void setUpRemoteView(int uid) {
+
+    }
+
+    private void initializeEngine() {
+
+    }
+
+    private void setUpLocalView() {
+
+    }
+
+    private void joinChannel() {
 
     }
 
